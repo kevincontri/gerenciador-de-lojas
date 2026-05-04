f22bac1ebf27e859c464ec5dcd698e16ec27312d
kevincontri <kevin.lucas.contri@gmail.com>
2026-05-04 08:52:05 -0300

    Phase 3 finished, all 'Loja' class extensions added

---
+++ Bijuteria.java
@@ -0,0 +1,22 @@
+class Bijuteria extends Loja {
+  double metaVendas;
+
+  public Bijuteria(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco,
+      Data dataFundacao, double metaVendas) {
+    super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+    this.metaVendas = metaVendas;
+  }
+
+  public double getMetaVendas() {
+    return metaVendas;
+  }
+
+  public void setMetaVendas(double metaVendas) {
+    this.metaVendas = metaVendas;
+  }
+
+  @Override
+  public String toString() {
+    return super.toString() + "\nMeta de Vendas: " + metaVendas;
+  }
+}
---
+++ Vestuario.java
@@ -0,0 +1,22 @@
+class Vestuario extends Loja {
+  boolean produtosImportados;
+
+  public Vestuario(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco,
+      Data dataFundacao, boolean produtosImportados) {
+    super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+    this.produtosImportados = produtosImportados;
+  }
+
+  public boolean getProdutosImportados() {
+    return produtosImportados;
+  }
+
+  public void setProdutosImportados(boolean produtosImportados) {
+    this.produtosImportados = produtosImportados;
+  }
+
+  @Override
+  public String toString() {
+    return super.toString() + "\nProdutos Importados: " + produtosImportados;
+  }
+}
